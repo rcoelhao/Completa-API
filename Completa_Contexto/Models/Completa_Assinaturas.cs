0bff96a68b1698801ff0c91e488e97520e6c19e2
rcoelho.rjb <rcoelho.rjb@gmail.com>
2020-11-14 16:44:34 -0300

    Publicado

--- Completa_Contexto/Models/Completa_Assinaturas.cs
+++ Completa_Contexto/Models/Completa_Assinaturas.cs
@@ -1,17 +1,19 @@
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace Completa_Contexto.Models
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
     public partial class Completa_Assinaturas
     {
         [Key]
         public int Ass_Id { get; set; }
 
-        public int Cli_Id { get; set; }
+        public int? Cli_Id { get; set; }
 
         [Column(TypeName = "image")]
         public byte[] Ass_image { get; set; }
-
     }
 }
